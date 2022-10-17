dad5660b6863afbcdc8ead941c87d71e3c83efdc
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2022-10-17 19:45:14 +0530

    Add router changes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName
@@ -12,17 +12,17 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma'
 });
 
-export default () => {
+export default ({ history }) => {
   return (
     <div>
-      <BrowserRouter>
-        <StylesProvider generateClassName={generateClassName}>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
             <Route path='/' component={Landing} />
           </Switch>
-        </StylesProvider>
-      </BrowserRouter>
+        </Router>
+      </StylesProvider>
     </div>
   );
 };
